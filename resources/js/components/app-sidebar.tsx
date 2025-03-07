b2fcc9e92c148acb50224db6ce0d5766191417e3
Bekir Gülmüş <bekir.gulmus@hotmail.com>
2025-03-08 00:24:20 +0300

    ff

---
+++ resources/js/components/app-sidebar.tsx
@@ -0,0 +1,141 @@
+import { NavFooter } from '@/components/nav-footer';
+import { NavMain } from '@/components/nav-main';
+import { NavUser } from '@/components/nav-user';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { type NavItem } from '@/types';
+import { Link } from '@inertiajs/react';
+import { BookOpen, Bot, Folder, Settings2, SquareTerminal } from 'lucide-react';
+import AppLogo from './app-logo';
+
+const mainNavItems: NavItem[] = [
+    // {
+    //     title: 'Dashboard',
+    //     url: '/dashboard',
+    //     icon: LayoutGrid,
+    // },
+    {
+        title: "Playground",
+        url: "#",
+        icon: SquareTerminal,
+        isActive: true,
+        items: [
+            {
+                title: "History",
+                url: "#",
+            },
+            {
+                title: "Starred",
+                url: "#",
+            },
+            {
+                title: "Settings",
+                url: "#",
+            },
+        ],
+    },
+    {
+        title: "Models",
+        url: "#",
+        icon: Bot,
+        items: [
+            {
+                title: "Genesis",
+                url: "#",
+            },
+            {
+                title: "Explorer",
+                url: "#",
+            },
+            {
+                title: "Quantum",
+                url: "#",
+            },
+        ],
+    },
+    {
+        title: "Documentation",
+        url: "#",
+        icon: BookOpen,
+        items: [
+            {
+                title: "Introduction",
+                url: "#",
+            },
+            {
+                title: "Get Started",
+                url: "#",
+            },
+            {
+                title: "Tutorials",
+                url: "#",
+            },
+            {
+                title: "Changelog",
+                url: "#",
+            },
+        ],
+    },
+    {
+        title: "Settings",
+        url: "#",
+        icon: Settings2,
+        items: [
+            {
+                title: "General",
+                url: "#",
+            },
+            {
+                title: "Team",
+                url: "#",
+            },
+            {
+                title: "Billing",
+                url: "#",
+            },
+            {
+                title: "Limits",
+                url: "#",
+            },
+        ],
+    },
+];
+
+const footerNavItems: NavItem[] = [
+    {
+        title: 'Repository',
+        url: 'https://github.com/laravel/react-starter-kit',
+        icon: Folder,
+    },
+    {
+        title: 'Documentation',
+        url: 'https://laravel.com/docs/starter-kits',
+        icon: BookOpen,
+    },
+];
+
+export function AppSidebar() {
+    return (
+        <Sidebar collapsible="icon" variant="inset">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <Link href="/dashboard" prefetch>
+                                <AppLogo />
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+
+            <SidebarContent>
+                <NavMain items={mainNavItems} />
+            </SidebarContent>
+
+            <SidebarFooter>
+                <NavFooter items={footerNavItems} className="mt-auto" />
+                <NavUser />
+            </SidebarFooter>
+        </Sidebar>
+    );
+}
